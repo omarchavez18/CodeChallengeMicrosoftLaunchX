3bb44cca4543ecce63a16180f48d2fd189df4cef
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-13 00:51:03 -0500

    se corrigio metodos

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,20 +2,20 @@ const studentService = require("./../services/studentService");
 const Reader = require("./../utils/Reader");
 
 class studentController {
-  static getStudents() {
-    const partners = Reader.readJsonFile("visualpartners.json");
-    return studentService.filterStudents(partners);
-  }
+    static getStudents() {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        return studentService.filterStudents(partners);
+    }
 
-  static getEmailsStudents(haveCertification) {
-    const partners = Reader.readJsonFile("visualpartners.json");
-    return studentService.getEmails(partners, haveCertification);
-  }
+    static getEmailsStudents(haveCertification) {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        return studentService.getEmails(partners, haveCertification);
+    }
 
-  static getStudentCreditList() {
-    const partners = Reader.readJsonFile("visualpartners.json");
-    return studentService.getStudentCredit(partners);
-  }
+    static getStudentCreditList() {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        return studentService.getStudentCredit(partners);
+    }
 }
 
 module.exports = studentController;
