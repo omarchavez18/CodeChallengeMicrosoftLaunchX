209acf40fef30d8b2ef6d1fa9658f345ce8ca15c
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-12 22:22:42 -0500

    se creo metodos de los requerimientos del controller

---
+++ controllers/studentController.js
@@ -0,0 +1,21 @@
+const studentService = require("./../services/studentService");
+const Reader = require("./../utils/Reader");
+
+class studentController {
+  static getStudents() {
+    const partners = Reader.readJsonFile("visualpartners.json");
+    return studentService.filterStudents(partners);
+  }
+
+  static getEmailsStudents(haveCertification) {
+    const partners = Reader.readJsonFile("visualpartners.json");
+    return studentService.getEmails(partners, haveCertification);
+  }
+
+  static getStudentCreditList() {
+    const partners = Reader.readJsonFile("visualpartners.json");
+    return studentService.getStudentCredit(partners);
+  }
+}
+
+module.exports = studentController;
