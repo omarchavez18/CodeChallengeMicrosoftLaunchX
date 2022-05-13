f4e520bf109404e3ae79f585fc5ca6fc23af0545
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-05-13 00:51:36 -0500

    se corrigio test

--- test/Reader.test.js
+++ test/Reader.test.js
@@ -2,8 +2,8 @@ const Reader = require("../utils/Reader");
 const partners = require("../visualpartners.json");
 
 describe("testing de Reader.js", () => {
-  test("read visualpartners.json file", () => {
-    const prueba = Reader.readJsonFile("visualpartners.json");
-    expect(prueba).toStrictEqual(partners);
-  });
+    test("read visualpartners.json file", () => {
+        const prueba = Reader.readJsonFile("visualpartners.json");
+        expect(prueba).toStrictEqual(partners);
+    });
 });
